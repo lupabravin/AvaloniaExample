fdf968c42c42face27b04314e1efea12d9879d4a
Lupa Bravin <l.bravinn@gmail.com>
2019-04-25 02:43:17 -0300

    First Commit

---
+++ Todo/ViewModels/AddItemViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Reactive;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using ReactiveUI;
+using Todo.Models;
+
+namespace Todo.ViewModels
+{
+    class AddItemViewModel : ViewModelBase
+    {
+        string description;
+        public string Description
+        {
+            get => description;
+            set => this.RaiseAndSetIfChanged(ref description, value);
+        }
+
+        public AddItemViewModel()
+        {
+
+            var okEnabled = this.WhenAnyValue(
+                x => x.Description,
+                x => !string.IsNullOrWhiteSpace(x));
+
+            Ok = ReactiveCommand.Create(
+                () => new TodoItem { Description = Description },
+                okEnabled);
+
+            Cancel = ReactiveCommand.Create(() => { });
+
+            Print = ReactiveCommand.Create<string>(WriteNumber);
+        }
+
+        public void WriteNumber(string number)
+        {
+            Description += number;
+        }
+
+        public ReactiveCommand<string, Unit> Print { get; }
+        public ReactiveCommand<Unit, TodoItem> Ok { get; }
+        public ReactiveCommand<Unit, Unit> Cancel { get; }
+    }
+}
---
+++ Todo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,54 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Text;
+using Todo.Models;
+using Todo.Services;
+
+namespace Todo.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+
+        ViewModelBase content;
+        public ViewModelBase Content
+        {
+            get => content;
+            private set => this.RaiseAndSetIfChanged(ref content, value);
+        }
+
+        public TodoListViewModel List { get; }
+
+        public MainWindowViewModel(Database db)
+        {
+            Content = List = new TodoListViewModel(db.GetItems());
+        }
+
+        public void AddItem()
+        {
+            var vm = new AddItemViewModel();
+
+            Observable.Merge(
+                vm.Ok,
+                vm.Cancel.Select(_ => (TodoItem)null))
+                .Take(1)
+                .Subscribe(model =>
+                {
+                    if (model != null)
+                    {
+                        List.Items.Add(model);
+                    }
+
+                    Content = List;
+                });
+            /*
+            Observable.Merge(
+               vm.Print
+                );
+                */
+            Content = vm;
+        }
+    }
+
+}
